914fba07550d910acb648d5b405b675b2f000924
Sanarix <KorsakovLA@mail.ru>
2023-05-13 15:32:44 +0300

    изменил стили, добавил firebase

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,5 +1,10 @@
 import App from './modules/App.js';
 import '../styles/index.css';
+import {initializeApp} from 'firebase/app';
+import firebaseConfig from '../../firebaseConfig.json';
+
+//For firebase hosting
+const init = initializeApp(firebaseConfig); 
 
 const app = new App();
 
